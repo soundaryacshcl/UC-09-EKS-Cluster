1316af510edaed62378f55fdd6f142819a1935ab
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-25 19:37:43 +0530

    helm error fix

--- modules/helm/main.tf
+++ modules/helm/main.tf
@@ -1,3 +1,21 @@
+# Get EKS Cluster and Auth Token
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+
 # Data Source: EKS Cluster Auth
 data "aws_eks_cluster_auth" "cluster" {
   name = var.cluster_id
--- modules/helm/variables.tf
+++ modules/helm/variables.tf
@@ -8,3 +8,9 @@ variable "vpc_id" {
 }
 variable "aws_region" {
 }
+
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+  default     = "533267119708"
+}
