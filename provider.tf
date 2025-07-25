9bf843599b4adfca12d9415e3ab176d286c59b1f
soundaryacshcl <soundaryacshcl@gmail.com>
2025-07-26 00:25:07 +0530

    fix: restore helm and kubernetes providers

--- provider.tf
+++ provider.tf
@@ -7,6 +7,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.31"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.9"
+    }
     http = {
       source = "hashicorp/http"
       #version = "2.1.0"
@@ -23,5 +27,23 @@ terraform {
 # Provider Block
 provider "aws" {
   region = var.region
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
